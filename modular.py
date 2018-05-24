937e0d90cffcc956f8c72dbb99d0592cdbd733b2
Patryk Biel <p.biel@fakro.pl>
2018-05-24 10:56:37 +0200

    Initial commit

---
+++ modular.py
@@ -0,0 +1,79 @@
+#!/usr/local/bin env python
+
+def modulo_inverse(a, b):
+    u = 1
+    w = a 
+    x = 0
+    z = b
+
+    while w != 0:
+        if w < z:
+            u, x = x, u
+            w, z = z, w  
+
+        q = w / z 
+        u = u - q * x
+        w = w - q * z
+    
+    if z != 1:
+        print "Brak rozwiazania"
+        
+        return None 
+
+    if x < 0:
+        x = x + b 
+
+    print "Rowiazanie: {}".format(x)
+    return x 
+
+def euclidean_alghoritm(a, b, nwd, x2, x1, x, y2, y1, y, q, r):
+    if r == 0:
+        return [nwd, y]
+    a = b
+    b = r
+
+    x = x2 - q*x1
+    x2 = x1
+    x1 = x
+
+    y = y2 - q*y1
+    y2 = y1
+    y1 = y
+
+    nwd = r
+    q = a/b
+    r = a-q*b
+
+    return euclidean_alghoritm(a, b, nwd, x2, x1, x, y2, y1, y, q, r)
+
+def calculate_euclidean(a, b, print_output=False):
+    if a == None:
+        raise ValueError("A cannot be None.")
+    
+    if b == None: 
+        raise ValueError("B cannot be None.")
+
+    if b > a:
+        tmp = a 
+        a = b
+        b = tmp
+
+    q = a/b
+    r = a - q*b
+    nwd = b
+    x2 = 1
+    x1 = 0
+    y2 = 0
+    y1 = 1
+    x = 1
+    y = y2 - (q-1)*y1
+    
+    nwd, y =  euclidean_alghoritm(a, b, nwd, x2, x1, x, y2, y1, y, q, r)
+
+    if print_output == True:
+        print ("NWD({}, {}) = {}").format(a, b, nwd)
+        
+        if nwd == 1:
+            print("{} * {} mod {} == 1").format(b, y, a)
+
+    return nwd, y
